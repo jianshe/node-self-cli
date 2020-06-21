c265139476d3d4afe2bb2f72af2d0958d87d85d7
jianshe <zhangshi789wei@163.com>
2020-06-22 00:29:46 +0800

    promise use

---
+++ api/09-red-green-yellow-light.js
@@ -0,0 +1,17 @@
+function sleep(duration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve,duration);
+  })
+}
+async function changeColor(duration, color) {
+  document.getElementById("traffic-light").style.background = color;
+  await sleep(duration);
+}
+async function main() {
+  while(true) {
+    await changeColor(3000, "green");
+    await changeColor(1000, "yellow");
+    await changeColor(2000, "red")
+  }
+}
+main();
